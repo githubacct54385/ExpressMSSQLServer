014432a77938c17fff42d90df31ca08ebdcbb089
Alex Barke <alex.barke@csintelligence.asia>
2020-02-18 15:24:17 +0800

    add new graphql schema methods delete and update

--- schema.js
+++ schema.js
@@ -1,13 +1,13 @@
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt,
   GraphQLSchema,
   GraphQLList,
-  GraphQLNonNull
+  GraphQLNonNull,
+  GraphQLBoolean
 } = require("graphql");
-
-const { getUsers } = require("./userMethods");
+const validator = require("validator");
+const { getUsers, addUser, deleteUser, updateUser } = require("./userMethods");
 
 // User Type
 const UserType = new GraphQLObjectType({
@@ -17,7 +17,33 @@ const UserType = new GraphQLObjectType({
     Name: { type: GraphQLString },
     Email: { type: GraphQLString },
     CreatedAt: { type: GraphQLString },
-    UpdatedAt: { type: GraphQLString }
+    UpdatedAt: { type: GraphQLString },
+    Error: { type: GraphQLString }
+  })
+});
+
+const AddUserPayload = new GraphQLObjectType({
+  name: "AddUserPayload",
+  fields: () => ({
+    HasError: { type: GraphQLBoolean },
+    Field: { type: GraphQLString },
+    Msg: { type: GraphQLString }
+  })
+});
+
+const DeleteUserPayload = new GraphQLObjectType({
+  name: "DeleteUserPayload",
+  fields: () => ({
+    HasError: { type: GraphQLBoolean },
+    Msg: { type: GraphQLString }
+  })
+});
+
+const UpdateUserPayload = new GraphQLObjectType({
+  name: "UpdateUserPayload",
+  fields: () => ({
+    HasError: { type: GraphQLBoolean },
+    Msg: { type: GraphQLString }
   })
 });
 
@@ -34,6 +60,124 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+// Mutation
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    updateUser: {
+      type: UpdateUserPayload,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString)
+        },
+        name: {
+          type: GraphQLString
+        },
+        email: {
+          type: GraphQLString
+        }
+      },
+      async resolve(parentValue, args) {
+        if (args.id === null || args.id === "") {
+          return {
+            HasError: true,
+            Msg: "Id argument cannot be empty or null"
+          };
+        }
+
+        if (!validator.isUUID(args.id)) {
+          return {
+            HasError: true,
+            Msg: "Id is not a Guid."
+          };
+        }
+
+        if (!args.name && !args.email) {
+          return {
+            HasError: true,
+            Msg:
+              "Neither name or email was selected for update.  You must choose at least one."
+          };
+        }
+
+        const updateResult = await updateUser(args.id, args.name, args.email);
+        if (updateResult.success) {
+          return { HasError: false, Msg: "" };
+        } else {
+          return { HasError: true, Msg: updateResult.msg };
+        }
+      }
+    },
+    deleteUser: {
+      type: DeleteUserPayload,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString)
+        }
+      },
+      async resolve(parentValue, args) {
+        if (args.id === null || args.id === "") {
+          return {
+            HasError: true,
+            Msg: "Id argument cannot be empty or null."
+          };
+        }
+        const { success, msg } = await deleteUser(args.id);
+        if (success) {
+          return { HasError: false, Msg: "" };
+        } else {
+          return { HasError: true, Msg: msg };
+        }
+      }
+    },
+    addUser: {
+      type: AddUserPayload,
+      args: {
+        name: {
+          type: new GraphQLNonNull(GraphQLString)
+        },
+        email: {
+          type: new GraphQLNonNull(GraphQLString)
+        }
+      },
+      async resolve(parentValue, args) {
+        // input validation
+        if (args.name === null || args.name === "") {
+          return {
+            HasError: true,
+            Field: "Name",
+            Msg: "Name argument cannot be empty."
+          };
+        }
+
+        if (args.email === null || args.email === "") {
+          return {
+            HasError: true,
+            Field: "Email",
+            Msg: "Name argument cannot be empty."
+          };
+        }
+
+        if (validator.isEmail(args.email) === false) {
+          return {
+            HasError: true,
+            Field: "Email",
+            Msg: "Email argument is not an email."
+          };
+        }
+
+        const { user } = await addUser(args.name, args.email);
+        if (user !== null) {
+          return { HasError: false, Field: "", Msg: "" };
+        } else {
+          return { HasError: true, Field: "Unknown", Msg: "Server Error!" };
+        }
+      }
+    }
+  }
+});
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 });
--- userMethods.js
+++ userMethods.js
@@ -2,6 +2,12 @@ const { sequelize, User } = require("./sequelize");
 const moment = require("moment");
 const { uuid } = require("uuidv4");
 
+function getUtcNow() {
+  return moment()
+    .utc()
+    .format();
+}
+
 async function getUsers() {
   try {
     // connect and auth
@@ -18,22 +24,94 @@ async function getUsers() {
 
 async function addUser(name, email) {
   try {
+    const newId = uuid();
     // add user
     await User.create({
-      Id: uuid(),
+      Id: newId,
       Name: name,
       Email: email,
-      CreatedAt: moment()
-        .utc()
-        .format(),
-      UpdatedAt: moment()
-        .utc()
-        .format()
+      CreatedAt: getUtcNow(),
+      UpdatedAt: getUtcNow()
     });
-    return { success: true, msg: "" };
+
+    // find the user in the db
+    const users = await User.findAll({
+      attributes: ["Id", "Name", "Email", "CreatedAt", "UpdatedAt"],
+      where: { Id: newId }
+    });
+
+    if (users.length === 1) {
+      return { success: true, msg: "", user: users[0] };
+    } else {
+      return {
+        success: false,
+        msg: "Inserted however Find All method returned not 1 row.",
+        user: null
+      };
+    }
   } catch (error) {
-    return { success: false, msg: `Failed to create user.  Reason: ${error}` };
+    return {
+      success: false,
+      msg: `Failed to create user.  Reason: ${error}`,
+      user: null
+    };
   }
 }
 
-module.exports = { getUsers, addUser };
+async function updateUser(id, name, email) {
+  try {
+    const includeName = name !== null && name !== "";
+    const includeEmail = email !== null && email !== "";
+
+    if (includeName && !includeEmail) {
+      // update just name
+      await User.update(
+        {
+          Name: name,
+          UpdatedAt: getUtcNow()
+        },
+        { where: { Id: id } }
+      );
+    } else if (!includeName && includeEmail) {
+      // update just email
+      await User.update(
+        {
+          Email: email,
+          UpdatedAt: getUtcNow()
+        },
+        { where: { Id: id } }
+      );
+    } else {
+      // update both name and email
+      await User.update(
+        {
+          Name: name,
+          Email: email,
+          UpdatedAt: getUtcNow()
+        },
+        { where: { Id: id } }
+      );
+    }
+
+    // find the updated user
+    const user = await User.findByPk(id);
+
+    return { success: true, msg: "", user: user };
+  } catch (error) {
+    return { success: false, msg: error, user: null };
+  }
+}
+
+async function deleteUser(id) {
+  try {
+    await User.destroy({ where: { Id: id } });
+    return { success: true, msg: "" };
+  } catch (error) {
+    return {
+      success: false,
+      msg: `Failed to delete user.  Reason: ${error}`
+    };
+  }
+}
+
+module.exports = { getUsers, addUser, deleteUser, updateUser };
